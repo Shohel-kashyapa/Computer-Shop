ca8f3410b00ce30b5f4c5e31a34a78def3719660
Shohel Kashyapa <shohelkashapa809@gmail.com>
2024-01-10 13:33:58 +0530

    menubar

--- main.js
+++ main.js
@@ -1,42 +1,70 @@
 // img slider
 
-let slider = document.querySelector('.slider .list');
-let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let dots = document.querySelectorAll('.slider .dots li');
+let slider = document.querySelector(".slider .list");
+let items = document.querySelectorAll(".slider .list .item");
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+let dots = document.querySelectorAll(".slider .dots li");
 
 let lengthItems = items.length - 1;
 let active = 0;
-next.onclick = function(){
-    active = active + 1 <= lengthItems ? active + 1 : 0;
-    reloadSlider();
-}
-prev.onclick = function(){
-    active = active - 1 >= 0 ? active - 1 : lengthItems;
-    reloadSlider();
-}
-let refreshInterval = setInterval(()=> {next.click()}, 3000);
-function reloadSlider(){
-    slider.style.left = -items[active].offsetLeft + 'px';
-    // 
-    let last_active_dot = document.querySelector('.slider .dots li.active');
-    last_active_dot.classList.remove('active');
-    dots[active].classList.add('active');
+next.onclick = function () {
+  active = active + 1 <= lengthItems ? active + 1 : 0;
+  reloadSlider();
+};
+prev.onclick = function () {
+  active = active - 1 >= 0 ? active - 1 : lengthItems;
+  reloadSlider();
+};
+let refreshInterval = setInterval(() => {
+  next.click();
+}, 3000);
+function reloadSlider() {
+  slider.style.left = -items[active].offsetLeft + "px";
+  //
+  let last_active_dot = document.querySelector(".slider .dots li.active");
+  last_active_dot.classList.remove("active");
+  dots[active].classList.add("active");
 
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(()=> {next.click()}, 3000);
-
-    
+  clearInterval(refreshInterval);
+  refreshInterval = setInterval(() => {
+    next.click();
+  }, 3000);
 }
 
 dots.forEach((li, key) => {
-    li.addEventListener('click', ()=>{
-         active = key;
-         reloadSlider();
-    })
-})
-window.onresize = function(event) {
+  li.addEventListener("click", () => {
+    active = key;
     reloadSlider();
+  });
+});
+window.onresize = function (event) {
+  reloadSlider();
 };
 
+const listItems = document.querySelectorAll(".sidebar-list li");
+
+listItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    let isActive = item.classList.contains("active");
+
+    listItems.forEach((el) => {
+      el.classList.remove("active");
+    });
+
+    if (isActive) item.classList.remove("active");
+    else item.classList.add("active");
+  });
+});
+
+const toggleSidebar = document.querySelector(".toggle-sidebar");
+const logo = document.querySelector(".logo-box");
+const sidebar = document.querySelector(".sidebar");
+
+toggleSidebar.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+logo.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
